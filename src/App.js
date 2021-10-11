cf3815fe8cdcf50d1855109ea04fd0d89898a5a4
dev-ashik <61373940+dev-ashik@users.noreply.github.com>
2021-10-11 14:13:18 +0800

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
-import React, { createContext, useContext, useState } from "react";
-import logo from "./logo.svg";
+import React, { createContext, useState } from "react";
 import "./App.css";
 import Header from "./Component/Header/Header";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Foodmenu from "./Component/Foodmenu/Foodmenu";
-import Fooddetail from "./Component/Fooddetail/Fooddetail";
-import Notfound from "./Component/Notfound/Notfound";
-import Footer from "./Component/Footer/Footer";
-import Foodcheckout from "./Component/Foodcheckout/Foodcheckout";
-import Blackfooter from "./Component/Blackfooter/Blackfooter";
-import Login from "./Component/Login/Login";
 import Menu from "./Component/Menu/Menu";
+import Login from "./Component/Login/Login";
 import fakefoodMenu from "./fakedata/Fakefood";
 import Chectout from "./Component/Checkout/Chectout";
 import Delivery from "./Component/Delivery/Delivery";
+import Foodmenu from "./Component/Foodmenu/Foodmenu";
+import Notfound from "./Component/Notfound/Notfound";
+import Fooddetail from "./Component/Fooddetail/Fooddetail";
+import Blackfooter from "./Component/Blackfooter/Blackfooter";
+import Foodcheckout from "./Component/Foodcheckout/Foodcheckout";
 import PrivateRoute from "./Component/PrivateRoute/PrivateRoute";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export const userContext = createContext();
 
 function App() {
-
   const [loggedInUser, setLoggedInUser] = useState({
-    name: ''
-  })
+    name: "",
+  });
 
   const [fCategory, setFCategory] = useState("lunch");
-  const [foodId, setFoodId] = useState("");
+
   const allLunch = fakefoodMenu.filter(
     (fakefood) => fakefood.category === fCategory
   );
   const lunchMenu = allLunch.slice(0, 6);
   const [currentfood, setCurrentfood] = useState(lunchMenu);
-  // console.log(currentfood);
+
   return (
     <userContext.Provider value={[loggedInUser, setLoggedInUser]}>
       <Router>
-          <Header/>
-          <Menu setCurrentfood={setCurrentfood} fCategory={fCategory} setFCategory={setFCategory}/>
+        <Header />
+        <Menu
+          setCurrentfood={setCurrentfood}
+          fCategory={fCategory}
+          setFCategory={setFCategory}
+        />
         <Switch>
-          {/* <Route path="/currentfood">
-            <Foodmenu currentfood="h"/>
-          </Route> */}
           <Route path="/food/:foodId">
             <Fooddetail></Fooddetail>
           </Route>
-          {/* <Route exact path="/">
-            <Menu></Menu>
-          </Route> */}
           <Route exact path="/">
             <Foodmenu currentfood={currentfood}></Foodmenu>
           </Route>
@@ -63,10 +58,9 @@ function App() {
             <Notfound></Notfound>
           </Route>
         </Switch>
-        <Chectout/>
+        <Chectout />
       </Router>
-      {/* <Footer></Footer>  */}
-      {/* <Blackfooter></Blackfooter>    */}
+      <Blackfooter></Blackfooter>
     </userContext.Provider>
   );
 }
--- src/Component/Delivery/Delivery.js
+++ src/Component/Delivery/Delivery.js
@@ -9,36 +9,48 @@ const Delivery = () => {
     <>
       <div className={styles.delivery}>
         <div>
-          <img className={styles.mapImage} src={map} alt="map" width="300" height="300" />
+          <img
+            className={styles.mapImage}
+            src={map}
+            alt="map"
+            width="300"
+            height="300"
+          />
         </div>
         <div className={styles.contactArea}>
-            <img className={styles.bike} src={bike} alt="bike" height="50" width="50"/>
-            <div className={styles.location}>
-                <div className={styles.locationTo}>
-                    <p>Your Location</p>
-                    <small>107 Rd No 8</small>
-                </div>
-                <div className={styles.locationFrom}>
-                    <p>Shop Address</p>
-                    <small>Gulshan Plaza Restaura GPA</small>
-                </div>
+          <img
+            className={styles.bike}
+            src={bike}
+            alt="bike"
+            height="50"
+            width="50"
+          />
+          <div className={styles.location}>
+            <div className={styles.locationTo}>
+              <p>Your Location</p>
+              <small>107 Rd No 8</small>
             </div>
-            <div className={styles.timeSection}>
-                <h2>09:30</h2>
-                <p>Estimated delivery time</p>
+            <div className={styles.locationFrom}>
+              <p>Shop Address</p>
+              <small>Gulshan Plaza Restaura GPA</small>
             </div>
-            <div className={styles.location}>
-                <div>
-                    <img src={helmet} alt="helmet" height="50" width="50"/>
-                </div>
-                <div>
-                    <h3>Hamim</h3>
-                    <p>Your raider</p>
-                </div>
+          </div>
+          <div className={styles.timeSection}>
+            <h2>09:30</h2>
+            <p>Estimated delivery time</p>
+          </div>
+          <div className={styles.location}>
+            <div>
+              <img src={helmet} alt="helmet" height="50" width="50" />
             </div>
-            <div className={styles.contactButton}>
-                <button >Contact</button>
+            <div>
+              <h3>Hamim</h3>
+              <p>Your raider</p>
             </div>
+          </div>
+          <div className={styles.contactButton}>
+            <button>Contact</button>
+          </div>
         </div>
       </div>
     </>
